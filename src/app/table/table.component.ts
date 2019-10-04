fafb4c845c999292ff92cebd1815ccdb105a95a6
fouaddev <fouaddev@bu.edu>
2019-10-04 13:43:00 -0400

    Added new features to manage Shopping Cart

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -6,40 +6,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./table.component.scss']
 })
 export class TableComponent implements OnInit {
-  books: any[];
-  title: any;
-  price: any;
-  qty: any;
-  total: any;
-  count: any;
-  orderTotal: any;
+  books: any[] = [];
+  defaultBook: any;
 
   constructor() {
-    this.title = 'Your Title Here';
-    this.price = 10.99;
-    this.qty = 1;
-    this.total = this.price * this.qty;
-    
-    this.books = [{
-      title: this.title,
-      price: this.price,
-      qty: this.qty,
-      total: this.total
-    }];
-    
-    this.count = this.books.length;
-
-    this.orderTotal = 0;
-    for(let i = 0; i < this.count; i++) {
-      this.orderTotal += this.books[i].total;
+    this.defaultBook = {
+      title: 'Your book Title',
+      price: 10.99,
+      qty: 1
     }
   }
 
   ngOnInit() {
-    
+
   }
 
-  addBook() {
-    
+  booksTotal(): number {
+    let total = 0;
+    for(let i = 0; i < this.books.length; i++) {
+      total += this.books[i].price * this.books[i].qty;
+    }
+
+    return total;
+  }
+
+  booksCount(): number {
+    let count = 0;
+    for(let i = 0; i < this.books.length; i++) {
+      count += this.books[i].qty;
+    }
+    return count;
+  }
+
+  addBook(): void {
+    this.books.push(this.defaultBook);
+
+    this.defaultBook = {
+      title: 'Your book Title',
+      price: 10.99,
+      qty: 1
+    };
+  }
+
+  deleteBook(e): void {
+    this.books.splice(e, 1);
   }
 }
